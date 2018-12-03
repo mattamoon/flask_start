6c68fd4b02ef3eae65d29944d6e11781fd466f9c
mattamoon <34408961+mattamoon@users.noreply.github.com>
2018-12-03 12:02:54 -0800

    Create routes.py

---
+++ app/users/routes.py
@@ -0,0 +1,28 @@
+from flask import render_template, url_for, flash, redirect, request, Blueprint
+from flask_login import login_user, current_user, logout_user, login_required
+from app import db, bcrypt
+from app.models import User #Import additional Objects here
+from app.users.forms import LoginForm #Import register form here if necessary
+
+users = Blueprint('users', __name__)
+
+@users.route("/login", methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('main.home'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('main.home'))
+        else:
+            flash('Login Unsuccessful. Please check email and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
+
+@users.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('main.home'))
